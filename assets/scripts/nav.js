145635b866d83782f55af5e2b3124121f393549d
Tom Wilkins <tgwilkins@btinternet.com>
2019-03-17 18:51:26 +0000

    nav toggle

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,11 +1,26 @@
 document.getElementById("hamburger").addEventListener("click", toggleNav);
 window.addEventListener('resize', restoreNav)
-var nav = document.querySelectorAll('.navigation li')
+var nav = document.querySelector('.navigation')
 
 function toggleNav() {
-    nav[0].style.display != "block" ? nav.forEach(li => li.style.display = "block") : nav.forEach(li => li.style.display = "none")
+    if ([...nav.classList].includes('hidemob')) {
+    nav.classList.remove('hidemob')
+    nav.classList.add('showmob')
+    }
+    else {
+        nav.classList.remove('showmob')
+    nav.classList.add('hidemob')
+    }
 }
 
 function restoreNav() {
-    window.innerWidth >= 600 ? nav.forEach(li => li.style.display = "inline") : nav.forEach(li => li.style.display = "none")
+    
+    if (window.innerWidth >= 800) {
+        nav.classList.remove('hidemob')
+        nav.classList.add('showmob')
+        }
+        else {
+            nav.classList.remove('showmob')
+        nav.classList.add('hidemob')
+        }
 }
